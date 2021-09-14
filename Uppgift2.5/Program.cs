fb3ebfa1df1bf709526141545e65dce740dbae99
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:40:17 +0200

    Uppgift2.5

---
+++ Uppgift2.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket år är det?");
+            int aar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Det är " + (2100-aar) + " år kvar till 2100.");
+        }
+    }
+}
