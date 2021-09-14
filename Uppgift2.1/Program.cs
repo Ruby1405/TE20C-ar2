28a7b8d46d441b9d05265125ed62f63b20a5d032
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:12:12 +0200

    Uppgift2.1

---
+++ Uppgift2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! var kommer du ifrån?");
+            string bostad = Console.ReadLine();
+            Console.WriteLine("Vad är ditt drömresmål?");
+            string resmaal = Console.ReadLine();
+            Console.WriteLine("Hej " + bostad + "-bo. Jag hoppas att du får resa till " + resmaal + ".");
+        }
+    }
+}
