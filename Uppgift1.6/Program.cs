4f9e8fb0892fd053fb5f554906b4e890bc79884d
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:03:41 +0200

    Uppgift 1.6

---
+++ Uppgift1.6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Critical error");
+            Console.WriteLine("C:\\github\\TE20C-arc2\\Uppgift1.6>...\nTermination request granted.");
+        }
+    }
+}
