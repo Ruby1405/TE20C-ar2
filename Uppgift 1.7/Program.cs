ef66b21372ad1d80fbe14601c97141b3d49d2927
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:05:49 +0200

    Uppgift 1.7

---
+++ Uppgift 1.7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift_1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\\n  /  \\\n / ”” \\\n/______\\");
+        }
+    }
+}
