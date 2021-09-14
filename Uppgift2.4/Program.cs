3693edf38cc83ec14b031e12503af53de7000e54
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:36:22 +0200

    Uppgift2.4

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Summan är " + (tal1+tal2) + " och produkten är " + (tal1*tal2));
+        }
+    }
+}
