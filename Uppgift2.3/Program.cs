38ebd84ca145312c7419b1f3a38d290cd45cf26d
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:32:16 +0200

    Uppgift2.3

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur högt kan du hoppa i meter?");
+            double hopp = 2.45 - double.Parse(Console.ReadLine());
+            Console.WriteLine("Rekordet i höjdhopp är " + hopp + " meter högre.");
+        }
+    }
+}
