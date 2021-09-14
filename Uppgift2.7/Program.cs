9c0ab55b52d9f43047363217fb07b5f9d9b854ca
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 11:05:38 +0200

    Uppgift2.7

---
+++ Uppgift2.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två ord på en rad");
+            string text = Console.ReadLine();
+            int indexspace = text.IndexOf(" ");
+            Console.WriteLine(text[(indexspace+1)..]+""+text[..indexspace]);
+        }
+    }
+}
