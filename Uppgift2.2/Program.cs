186552553d5be01835e29edf545bb22a22c51562
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:26:11 +0200

    Uppgift2.2

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket årtal är det i år?");
+            int aar = int.Parse(Console.ReadLine());
+            aar += 20;
+            Console.WriteLine("Om 20 år är året " + aar); 
+        }
+    }
+}
