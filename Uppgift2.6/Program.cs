4db19fedfe03009b051ec59c617810a0a8bc7b56
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-14 10:42:44 +0200

    Uppgift2.6

---
+++ Uppgift2.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Medelvärdet är " + ((tal1+tal2)/2));
+        }
+    }
+}
