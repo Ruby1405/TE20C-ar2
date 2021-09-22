50bff10d6061dccd5be1e911e7b731813c018c3b
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-09-22 10:30:36 +0200

    Uppgift2.15 och if satser

---
+++ Kapitel2/Uppgift2.15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift2._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sammlar in en mening på tre ord från användaren
+            Console.WriteLine("Skriv en mening med tre ord, avsluta med punkt.");
+            string text = Console.ReadLine();
+
+            //Lägger in indexing på mellanslag och punkten
+            int dotindex = text.IndexOf(".");
+            int spaceindex1 = text.IndexOf(" ");
+            int spaceindex2 = text.IndexOf(" ",spaceindex1+1);
+
+            //Skriver ut orden i omvänd ordning
+            Console.WriteLine($"{text[(spaceindex2+1)..dotindex]} {text[(spaceindex1+1)..spaceindex2]} {text[..spaceindex1]}.");
+        }
+    }
+}
---
+++ Kapitel3/IF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is six times nine? ");
+            if (Console.ReadLine()=="42")
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG YOU IMBISILE, IT'S 42!");
+            }
+
+            Console.Write("\nVad heter du? ");
+            Console.WriteLine($"Hej {Console.ReadLine().ToUpper()}!");
+        }
+    }
+}
