105280555c2ffb389ffa91a63472cc3999deb835
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-05 11:05:11 +0200

    gjort ett referns dokument för loopar

---
+++ Kapitel4/New folder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace New_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new int[10];
+            
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                break;
+            }
+
+            foreach (var item in bob)
+            {
+                
+            }
+
+            do{
+
+            }while(true);
+        }
+    }
+}
