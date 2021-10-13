c3f05063aaa77dc75762450e735e9202e1a079a3
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-13 10:30:40 +0200

    Rättning av prov samt skapande av mappen "egna projekt"

---
+++ PrrPrro/Prover/Prov1Rättning/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean restart = false;
+            do
+            {
+                Console.WriteLine("Nättolönekalkylator.\nVad heter du?");
+                string namn = Console.ReadLine(); //Frågar vad användaren heter
+
+                Console.WriteLine("Vad känar du? (i kronor)");
+                float brutto = float.Parse(Console.ReadLine()); //Frågar vad användaren känar brutto
+
+                //Kollar att svaret är giltigt
+                if (brutto > 10000 && brutto < 45000)
+                {
+                    Console.WriteLine("Vad har du för inkomstskatt? (i procent)");
+                    float skatt = float.Parse(Console.ReadLine());
+
+                    //Kollar att svaret är giltigt
+                    if (skatt > 10 && skatt < 45)
+                    {
+                        Console.WriteLine($"Du betalar {brutto * ((100 - skatt) / 100)}kr i skatt.\nVill du köra om? (ja/nej)");
+                    }
+                    else
+                    {
+                        //Om svaret inte var giligt förklarar systemet varför och frågar om användaren vill köra om
+                        Console.WriteLine("Programmet kan bara hantera skatt mellan 10% och 45%\nVill du köra om? (ja/nej)");
+                    }
+
+                }
+                else
+                {
+                    //Om svaret inte var giligt förklarar systemet varför och frågar om användaren vill köra om
+                    Console.WriteLine("Programmet kan bara hantera bruttoinkomster mellan 10000 och 45000\nVill du köra om? (ja/nej)");
+                }
+
+                if (Console.ReadLine() == "ja")//Kollar om användaren vill köra om
+                    restart = true;
+            } while (restart);//Startar om om användaren vill
+
+
+
+        }
+    }
+}
