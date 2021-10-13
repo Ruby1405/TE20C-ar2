29d5d8ad0fe862c3b57e5ff9209afd2212383aaa
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-13 11:07:30 +0200

    Rock Paper Scissors Lizzard Spock

---
+++ PrrPrro/EgnaProjekt/Random/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string retry = "uwu";
+            string[] options = {"Rock","Paper","Scissors","Lizzard","Spock"};
+            do{
+
+
+                Console.WriteLine("Do you wanna go again?");
+                retry = Console.ReadLine().ToLower();
+            }while(retry == "yes");
+        }
+    }
+}
