2a733172dec34608c60f79264164a4894bb53354
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-19 11:09:37 +0200

    2048

---
+++ PrrPrro/EgnaProjekt/2048/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("■■■■■■");
+            Console.WriteLine("■2048■");
+            Console.WriteLine("■■■■■■");
+            Console.WriteLine(" ◜‾‾‾‾‾‾‾‾◝");
+            Console.WriteLine("| 2048 |");
+            Console.WriteLine(" ◟_____◞");
+            
+        }
+    }
+}
---
+++ PrrPrro/EgnaProjekt/Mans/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ForLoop { 
+    class Program { 
+        static void Main(string[] args) { 
+            for (var i = 0; i <= 100000; i++) { 
+                Console.WriteLine($"{i}"); 
+            } 
+            Console.WriteLine($"Tick-count: {Environment.TickCount}"); 
+        } 
+    } 
+}
