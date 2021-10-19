ef93b241c6d730a99db8641dede1d2cfea36e715
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-19 10:22:03 +0200

    Forloop som jag redan kan

---
+++ PrrPrro/Kapitel4/ForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Räkna 1 till 10");
+
+            for (var i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"Loop nr {i}");
+            }
+
+
+        }
+    }
+}
