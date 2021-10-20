da35b965049e45208a374c03ae5c6f1aba039e37
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-20 09:51:34 +0200

    Sleepsort

---
+++ PrrPrro/EgnaProjekt/Sleepsort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sleepsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number to sleep sort");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Interpreted {number}");
+            for (var i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+}
