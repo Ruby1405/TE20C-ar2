f25cea1a40d6cefc972e0ec9f709e056ed49a60d
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2021-10-20 10:51:05 +0200

    Messing with arrays

---
+++ PrrPrro/EgnaProjekt/GameOfLife/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Game of life");
+
+            int[,] currentCells = new int[64,16];
+            //Console.WriteLine(currentCells.GetLength(0));
+            //Console.WriteLine(currentCells.GetLength(1));
+
+            while (true)
+            {
+                for (var x = 0; x < currentCells.GetLength(0); x++)
+                {
+                    for (var y = 0; y < currentCells.GetLength(1); y++)
+                    {
+                        Console.Write("O");
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
---
+++ PrrPrro/EgnaProjekt/TypeWrite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TypeWrite
+{
+    class Program
+    {
+        static void TypeWrite2(string text){
+            for(int i = 0; i < text.Length; i++){
+                Console.Write(text[i]);
+                if(text[i] == ",.!?\n".ToCharArray()){
+                    System.Threading.Thread.Sleep(500);
+                }else{
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+        }
+
+        static void TypeWrite(string text){
+            for(int i = 0; i < text.Length; i++){
+                Console.Write(text[i]);
+                if(text[i] == ',' || text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\n'){
+                    System.Threading.Thread.Sleep(500);
+                }else{
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            TypeWrite("Hello world! I am your maker.");
+            
+        }
+    }
+}
