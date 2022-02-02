89fd0cd69fced85fd598e57a6bb462b22694aed3
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2022-02-02 11:09:59 +0100

    2048 gamestate

---
+++ PrrPrro/Raylib/2048/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Raylib_cs;
+
+namespace _2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int windowX=800;
+            const int windowY=800;
+            Raylib.InitWindow(windowX,windowY,"Board");
+            Raylib.SetTargetFPS(60);
+
+            Random generator = new Random();
+
+            int[,] cell = new int[4,4];
+
+            
+            for (var i = 0; i < cell.GetLength(0); i++)
+            {
+                for (var j = 0; j < cell.GetLength(1); j++)
+                {
+                    cell[i,j]=0;
+                }
+            }
+
+
+            while(!Raylib.WindowShouldClose()){
+                
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
---
+++ PrrPrro/Raylib/NotCeleste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace NotCeleste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
