d4f32fe3db1c2a462cd4619bda4c94a907a3a7f8
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2022-02-16 09:37:22 +0100

    game will no longer crash at 4096

--- PrrPrro/Raylib/2048/Program.cs
+++ PrrPrro/Raylib/2048/Program.cs
@@ -30,7 +30,7 @@ namespace _2048
             {
                 for (var j = 0; j < cell.GetLength(1); j++)
                 {
-                    cell[i,j]=j;
+                    cell[i,j]=10;
                 }
             } */
 
@@ -243,7 +243,12 @@ namespace _2048
                 for (var i = 0; i < cell.GetLength(0); i++){for (var j = 0; j < cell.GetLength(1); j++){
                     if(cell[i,j]>0)
                     {
+                        if(cell[i,j]<12)
                         Raylib.DrawRectangle(i*200+1,j*200+1,198,198,new Color((256/11)*cell[i,j]-1,0,100,255));
+                        else if (cell[i,j]<23)
+                        Raylib.DrawRectangle(i*200+1,j*200+1,198,198,new Color(255,(256/11)*(cell[i,j]-11)-1,100,255));
+                        else
+                        Raylib.DrawRectangle(i*200+1,j*200+1,198,198,new Color(255,255,100,255));
                         Raylib.DrawText($"{Math.Pow(2,cell[i,j])}",5+i*200+10,75+j*200,50,Color.WHITE);
                     }
                     //Raylib.DrawText($"X{i} Y{j} Value{cell[i,j]}",5+i*200+10,120+j*200,20,Color.RED);//debug
