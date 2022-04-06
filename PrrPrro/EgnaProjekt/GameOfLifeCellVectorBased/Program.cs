a0ac888a5ad21526cceaf1e44e98ff3764cdc04b
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2022-04-06 14:11:02 +0200

    Started new game of life that stores live cells

---
+++ PrrPrro/EgnaProjekt/GameOfLifeCellVectorBased/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Raylib_cs;
+using System.Numerics;
+
+namespace GameOfLifeCellVectorBased
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
