50b4e43ed1ae06394a21a2408982a173e56b78ed
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2022-04-08 11:06:11 +0200

    Calc done

--- PrrPrro/EgnaProjekt/GameOfLifeCellVectorBased/Program.cs
+++ PrrPrro/EgnaProjekt/GameOfLifeCellVectorBased/Program.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 using Raylib_cs;
 using System.Numerics;
+using System.Collections.Generic;
 
 namespace GameOfLifeCellVectorBased
 {
     class Program
     {
+        static (int, int) window = (800,800);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Raylib.InitWindow(window.Item1,window.Item2, "Game Of Life");
+            Raylib.SetTargetFPS(60);            
+
+            float zoom = 1;
+
+            List<Vector2> cells = new List<Vector2>();
+            List<Vector2> newCells = new List<Vector2>();
+
+            while(!Raylib.WindowShouldClose()){
+                foreach (var cell in cells)
+                {
+                    for (var X = -1; X < 1; X++)
+                    {
+                        for (var Y = -1; Y < 1; Y++)
+                        {
+                            newCells.Add(new Vector2(cell.X+X,cell.Y+Y));
+                        }
+                    }
+                }
+                cells.Clear();
+                int newCellIndex=0;
+                foreach (var cell in newCells)
+                {
+                    int newCellCount=0;
+                    foreach (var cell2 in newCells)
+                    {
+                        if (cell==cell2)
+                        {
+                            newCellCount++;
+                            newCells.Remove(cell2);
+                        }
+                    }
+                    if(newCellCount>2)
+                    {
+                        cells.Add(cell);
+                    }
+                    newCellIndex++;
+                }
+                newCells.Clear();
+            }
         }
     }
 }
