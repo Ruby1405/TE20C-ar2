fd0d74deb831bfa25ec2cf4f7ebaafc09143ce63
Benjamin Calmerman <Calmerman.Benjamin@elev.ga.ntig.se>
2022-04-20 11:09:40 +0200

    fixed overpopulation & started cell drawing

--- PrrPrro/EgnaProjekt/GameOfLifeCellVectorBased/Program.cs
+++ PrrPrro/EgnaProjekt/GameOfLifeCellVectorBased/Program.cs
@@ -13,42 +13,65 @@ namespace GameOfLifeCellVectorBased
             Raylib.InitWindow(window.Item1,window.Item2, "Game Of Life");
             Raylib.SetTargetFPS(60);            
 
-            float zoom = 1;
+            //Initiate the zoom level of the window
+            float zoom = 100;
 
+            //Initiate the camera position
+            Vector2 cameraPos = new Vector2(0,0);
+
+            //Initiate a list of current cells and one of potential new cells and new cells
             List<Vector2> cells = new List<Vector2>();
+            List<Vector2> potentialCells = new List<Vector2>();
             List<Vector2> newCells = new List<Vector2>();
 
+            //Game loop
             while(!Raylib.WindowShouldClose()){
+
+                //For every cell add all cells in a 3 by 3 area to potential new cells
                 foreach (var cell in cells)
                 {
                     for (var X = -1; X < 1; X++)
                     {
                         for (var Y = -1; Y < 1; Y++)
                         {
-                            newCells.Add(new Vector2(cell.X+X,cell.Y+Y));
+                            potentialCells.Add(new Vector2(cell.X+X,cell.Y+Y));
                         }
                     }
                 }
-                cells.Clear();
-                int newCellIndex=0;
-                foreach (var cell in newCells)
+
+                //Count the amount of occurences of a cell in the potential cells list
+                foreach (var cell in potentialCells)
                 {
                     int newCellCount=0;
-                    foreach (var cell2 in newCells)
+                    foreach (var cell2 in potentialCells)
                     {
                         if (cell==cell2)
                         {
                             newCellCount++;
-                            newCells.Remove(cell2);
+                            potentialCells.Remove(cell2);
                         }
                     }
-                    if(newCellCount>2)
+
+                    //Refer to the rules of game of life
+                    if((newCellCount == 3 && !cells.Contains(cell)) || (newCellCount > 2 && newCellCount < 5 && cells.Contains(cell)))
                     {
-                        cells.Add(cell);
+                        newCells.Add(cell);
                     }
-                    newCellIndex++;
                 }
+                //Set cells to newCells and clear potential and new
+                cells = newCells;
                 newCells.Clear();
+                potentialCells.Clear();
+
+                foreach (var cell in cells)
+                {
+                    Raylib.DrawRectangle(
+                    (int)(cell.X*zoom/2),
+                    (int)(cell.Y*zoom/2),
+                    (int)((zoom/2)-(zoom/20)),
+                    (int)((zoom/2)-(zoom/20)),
+                    new Color(255,0,0,255));
+                }
             }
         }
     }
